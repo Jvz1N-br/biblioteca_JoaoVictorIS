3bd37c5f305ce2439f4dd9d2611c983bd2476f03
Jvz1N-br <inaciodasilvajoaovictor3@gmail.com>
2026-03-19 10:30:24 -0300

    s

--- documento sistemas 01/bibliotecaM2/routes/livros.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const router = express.router();
-
-const LivrosController = require('../controllers/livros.controllers');
-
-router.get('/'), livrosController.listarLivros
