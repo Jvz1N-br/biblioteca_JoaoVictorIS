fe55064d3eb8f587f4a38314f0b4527b1ec3fa9c
Jvz1N-br <inaciodasilvajoaovictor3@gmail.com>
2026-03-23 10:22:50 -0300

    s

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const livrosRoutes = require('./src/routes/livros.routes');
-
+const routes = require('./src/routes/index.routes');
 const app = express();
 
 app.use(express.json());
 
-app.use('/livros', livrosRoutes);
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
-const PORT = 3000;
+// Utilizando as rotas
+app.use(routes);
 
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`[SERVIDOR] Biblioteca ondeline em http://localhost:${PORT}`);
+  console.log(`[SERVIDOR]: Biblioteca online em http://localhost:${PORT}`);
 });
--- src/services/livros.services.js
+++ src/services/livros.services.js
@@ -9,7 +9,7 @@ const acervo = [
     id: 2,
     titulo: 'Altered Carbon',
     autor: 'Richard K. Morgan',
-    disponivel: true,
+    disponivel: false,
   },
   {
     id: 3,
@@ -19,8 +19,33 @@ const acervo = [
   },
 ];
 
+// Lista todos os livros do acervo
 const listarTodosLivros = async () => {
   return acervo;
 };
 
-module.exports = { listarTodosLivros };
+// Busca um livro específico pelo ID
+const buscarLivroPorId = async (id) => {
+  const livro = acervo.find((l) => l.id === Number(id));
+  // Regra de negócio: se não existe, retorna null.
+  // O Controller decide o que fazer com o null.
+  return livro || null;
+};
+
+// Criar um novo livro no acervo
+const criarLivro = async ({ titulo, autor }) => {
+  // Regra de negócio: título e autor são obrigatórios
+  if (!titulo || !autor) {
+    throw new Error('Título e autor são obrigatórios.');
+  }
+  const novoLivro = {
+    id: acervo.length + 1,
+    titulo,
+    autor,
+    disponivel: true,
+  };
+  acervo.push(novoLivro);
+  return novoLivro;
+};
+
+module.exports = { listarTodosLivros, buscarLivroPorId, criarLivro };
