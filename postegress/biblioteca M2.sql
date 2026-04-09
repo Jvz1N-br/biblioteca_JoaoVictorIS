eebe7e8f3dab1685f24bdcba97d10142fa9ab37a
Jvz1N-br <inaciodasilvajoaovictor3@gmail.com>
2026-04-09 11:20:03 -0300

    s

---
+++ postegress/biblioteca M2.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuario (nome, matricula, email, role) VALUES
+	('João V', 222020, 'joao@gmail.com', 'aluno'),
+	('Anderson D', 254020, 'andersonD7@gmail.com', 'admin'),
+	('arthur', 256780, 'ath@gmail.com', 'funcionario');
